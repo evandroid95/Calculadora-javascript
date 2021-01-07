5446504dfbb0c0f36340849b1d909a2ea70f9c79
evandroid95 <evandrosantos342@outlook.com>
2021-01-07 19:42:01 -0300

    second projeto

---
+++ Desktop/PORTIFOLIO/PASTA CALCULO DE RAIO.c/CALCULO DE RAIO.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.1415
+main () {
+ 
+	float raio;
+	
+	printf("Escreva o numero:");
+	scanf("%f",&raio);
+	float area=PI*raio*raio;
+	printf("area:%.2f",area);
+	
+	scanf("%f" ,&raio);
+	
+	
+	
+	system("PAUSE");
+	
+return 0;
+}
+
+
+
